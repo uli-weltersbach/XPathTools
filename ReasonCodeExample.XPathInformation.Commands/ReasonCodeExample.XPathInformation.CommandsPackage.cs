6d5094538b667ed8ec54d833f9436bb392ecf0ff
Uli Weltersbach <uli.weltersbach@gmail.com>
2013-05-06 21:07:31 +0200

    Minimalistic working implementation

--- ReasonCodeExample.XPathInformation.Commands/ReasonCodeExample.XPathInformation.CommandsPackage.cs
+++ ReasonCodeExample.XPathInformation.Commands/ReasonCodeExample.XPathInformation.CommandsPackage.cs
@@ -1,33 +1,32 @@
-﻿using Microsoft.VisualStudio.Shell;
-using Microsoft.VisualStudio.Shell.Interop;
-using System;
+﻿using System;
 using System.ComponentModel.Design;
-using System.Globalization;
 using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
 
 namespace ReasonCodeExample.XPathInformation.Commands
 {
-    /// <summary>
-    /// This is the class that implements the package exposed by this assembly.
-    ///
-    /// The minimum requirement for a class to be considered a valid package for Visual Studio
-    /// is to implement the IVsPackage interface and register itself with the shell.
-    /// This package uses the helper classes defined inside the Managed Package Framework (MPF)
-    /// to do it: it derives from the Package class that provides the implementation of the 
-    /// IVsPackage interface and uses the registration attributes defined in the framework to 
-    /// register itself and its components with the shell.
-    /// </summary>
-    // This attribute tells the PkgDef creation utility (CreatePkgDef.exe) that this class is
-    // a package.
     [PackageRegistration(UseManagedResourcesOnly = true)]
-    // This attribute is used to register the information needed to show this package
-    // in the Help/About dialog of Visual Studio.
     [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
-    // This attribute is needed to let the shell know that this package exposes some menus.
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(Constants.PackageID)]
     public sealed class CommandsPackage : Package
     {
+        private readonly IMenuCommandService _menuCommandService;
+        private readonly IVsStatusbar _statusbar;
+
+        public CommandsPackage()
+            : this(null, null)
+        {
+        }
+
+        public CommandsPackage(IMenuCommandService menuCommandService, IVsStatusbar statusbar)
+        {
+            _menuCommandService = menuCommandService ?? (IMenuCommandService)GetService(typeof(IMenuCommandService));
+            _statusbar = statusbar ?? (IVsStatusbar)GetService(typeof(IVsStatusbar));
+        }
+
         /// <summary>
         /// Initialization of the package; this method is called right after the package is sited, so this is the place
         /// where you can put all the initialization code that rely on services provided by VisualStudio.
@@ -35,39 +34,16 @@ namespace ReasonCodeExample.XPathInformation.Commands
         protected override void Initialize()
         {
             base.Initialize();
-            OleMenuCommandService menuCommandService = GetService(typeof(IMenuCommandService)) as OleMenuCommandService;
-            if (menuCommandService == null)
-                return;
-
             CommandID menuCommandID = new CommandID(Guid.Parse(Constants.MenuGroupID), Constants.SaveCommandSortOrder);
-            OleMenuCommand menuCommand = new OleMenuCommand(MenuCommandCallback, menuCommandID);
-            menuCommandService.AddCommand(menuCommand);
+            MenuCommand menuCommand = new OleMenuCommand(CopyStatusBarTextToClipBoard, menuCommandID);
+            _menuCommandService.AddCommand(menuCommand);
         }
 
-        /// <summary>
-        /// This function is the callback used to execute a command when the a menu item is clicked.
-        /// See the Initialize method to see how the menu item is associated to this function using
-        /// the OleMenuCommandService service and the MenuCommand class.
-        /// </summary>
-        private void MenuCommandCallback(object sender, EventArgs e)
+        private void CopyStatusBarTextToClipBoard(object sender, EventArgs e)
         {
-            // Show a Message Box to prove we were here
-            IVsUIShell uiShell = (IVsUIShell)GetService(typeof(SVsUIShell));
-            Guid clsid = Guid.Empty;
-            int result;
-            Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(uiShell.ShowMessageBox(
-                       0,
-                       ref clsid,
-                       "ReasonCodeExample.XPathInformation.Commands",
-                       string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.ToString()),
-                       string.Empty,
-                       0,
-                       OLEMSGBUTTON.OLEMSGBUTTON_OK,
-                       OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST,
-                       OLEMSGICON.OLEMSGICON_INFO,
-                       0,        // false
-                       out result));
+            string text;
+            _statusbar.GetText(out text);
+            Clipboard.SetText(text);
         }
-
     }
 }
