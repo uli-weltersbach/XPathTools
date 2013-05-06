92277b362be739e3e14a50537ec99200959668c1
Uli Weltersbach <uli.weltersbach@gmail.com>
2013-05-06 20:07:30 +0200

    Commands project added

---
+++ ReasonCodeExample.XPathInformation.Commands/IDs.cs
@@ -0,0 +1,13 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace ReasonCodeExample.XPathInformation.Commands
+{
+    internal static class IDs
+    {
+        public const string PackageID = "253aa665-a779-4716-9ded-5b0c2cb66710";
+        public const string MenuGroupID = "2a859db4-750c-4267-b96f-844f20ce9e7b";
+        public const int SaveCommandID = 0x100;
+    };
+}
