558f697c6999037f8bfdd2235e28cf6d3398368b
Uli Weltersbach <uli.weltersbach@gmail.com>
2013-05-07 21:27:07 +0200

    Moved files to primary project.

--- ReasonCodeExample.XPathInformation/Commands/ReasonCodeExample.XPathInformation.CommandsPackage.cs
+++ ReasonCodeExample.XPathInformation/Commands/ReasonCodeExample.XPathInformation.CommandsPackage.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.ComponentModel.Design;
 using System.Runtime.InteropServices;
-using System.Windows.Forms;
+using System.Windows;
 using Microsoft.VisualStudio.Shell;
 
 namespace ReasonCodeExample.XPathInformation.Commands
 {
     [PackageRegistration(UseManagedResourcesOnly = true)]
-    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)]
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(Constants.PackageID)]
     public sealed class CommandsPackage : Package
@@ -29,18 +28,14 @@ namespace ReasonCodeExample.XPathInformation.Commands
 
         private void CopyXPathToClipBoard(object sender, EventArgs e)
         {
-            MenuCommand menuCommand = sender as MenuCommand;
-            if (menuCommand == null)
-                return;
             Clipboard.SetText(_repository.Get());
         }
 
         private void SetCommandVisibility(object sender, EventArgs e)
         {
             MenuCommand menuCommand = sender as MenuCommand;
-            if (menuCommand == null)
-                return;
-            menuCommand.Visible = !string.IsNullOrEmpty(_repository.Get());
+            if (menuCommand != null)
+                menuCommand.Visible = !string.IsNullOrEmpty(_repository.Get());
         }
     }
 }
