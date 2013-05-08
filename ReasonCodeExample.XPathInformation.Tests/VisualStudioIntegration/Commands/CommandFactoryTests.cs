4b6e3e7720786d4b3c6e595361452d07396b3d36
Uli Weltersbach <uli.weltersbach@gmail.com>
2013-05-08 16:11:20 +0200

    CommandFactoryTests added.

---
+++ ReasonCodeExample.XPathInformation.Tests/VisualStudioIntegration/Commands/CommandFactoryTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VsSDK.UnitTestLibrary;
+using NUnit.Framework;
+using ReasonCodeExample.XPathInformation.VisualStudioIntegration.Commands;
+
+namespace ReasonCodeExample.XPathInformation.Tests.VisualStudioIntegration.Commands
+{
+    [TestFixture]
+    public class CommandFactoryTests
+    {
+        private const int S_OK = 0;
+
+        [Test]
+        public void CanCreateInstance()
+        {
+            CommandFactory commandFactory = new CommandFactory();
+        }
+
+        [Test]
+        public void ImplementsVisualStudioPackageInterface()
+        {
+            // Assert
+            Assert.That(new CommandFactory(), Is.InstanceOf<IVsPackage>());
+        }
+
+        [Test]
+        public void CanSetSite()
+        {
+            // Arrange
+            IVsPackage commandFactory = new CommandFactory();
+            OleServiceProvider serviceProvider = OleServiceProvider.CreateOleServiceProviderWithBasicServices();
+
+            // Act
+            int actualResult = commandFactory.SetSite(serviceProvider);
+
+            // Assert
+            Assert.That(S_OK, Is.EqualTo(actualResult), "SetSite(serviceProvider) did not return S_OK");
+        }
+
+        [Test]
+        public void CanResetSite()
+        {
+            // Arrange
+            IVsPackage commandFactory = new CommandFactory();
+            
+            // Act
+            int actualResult = commandFactory.SetSite(null);
+
+            // Assert
+            Assert.That(S_OK, Is.EqualTo(actualResult), "SetSite(null) did not return S_OK");
+        }
+    }
+}
