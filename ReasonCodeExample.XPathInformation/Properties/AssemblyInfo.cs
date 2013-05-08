3be2b9c0e9f2ca9d8341af47e333759bbd5c1d72
Uli Weltersbach <uli.weltersbach@gmail.com>
2013-05-08 08:47:30 +0200

    Keys added for signing the assemblies.

--- ReasonCodeExample.XPathInformation/Properties/AssemblyInfo.cs
+++ ReasonCodeExample.XPathInformation/Properties/AssemblyInfo.cs
@@ -8,4 +8,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: InternalsVisibleTo("ReasonCodeExample.XPathInformation.Tests")]
+[assembly: InternalsVisibleTo("ReasonCodeExample.XPathInformation.Tests,PublicKey=0024000004800000940000000602000000240000525341310004000001000100e35cb7428edc7672eb04d0a0d26120fb5f69f0e7196b43f8c53478425763c16da649ec48765fcb8f1672256a6a0482746c8b74444fa1128117b836932fb6b2913296d9627a67a20d93554efa9fa8f2dc5c3805d5991678683a2db6e8b91a5924609729b431bd25d0b2dbb93453f99a101bf8bd770d1f1d53d17fe1ae4581b9f1")]
