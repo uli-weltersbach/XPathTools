3669a3cc468d4f47e28605cc84eae781d61465b0
Uli Weltersbach <uli.weltersbach@gmail.com>
2013-05-08 08:00:58 +0200

    XPathRepositoryTests added.

---
+++ ReasonCodeExample.XPathInformation.Tests/XPathRepositoryTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace ReasonCodeExample.XPathInformation.Tests
+{
+    [TestFixture]
+    public class XPathRepositoryTests
+    {
+        [Test]
+        public void XPathIsStored()
+        {
+            // Arrange
+            string expectedValue = "value";
+            XPathRepository repository = new XPathRepository();
+            XPathRepository otherRepository = new XPathRepository();
+
+            // Act
+            repository.Put(expectedValue);
+
+            // Assert
+            Assert.That(otherRepository.Get(), Is.EqualTo(expectedValue));
+        }
+    }
+}
