c1464667ac9d4b34186a41762117e726122832ed
Uli Weltersbach <uli.weltersbach@gmail.com>
2013-11-29 21:19:50 +0100

    Cleanup

--- ReasonCodeExample.XPathInformation.Tests/Formatters/DistinctPathFormatterTests.cs
+++ ReasonCodeExample.XPathInformation.Tests/Formatters/DistinctPathFormatterTests.cs
@@ -15,7 +15,7 @@ namespace ReasonCodeExample.XPathInformation.Tests.Formatters
         [TestCase("<a><b /><b /><b /><b><c id='same' name='unique'/><c id='same' /></b></a>", 5, "/a/b/c[@name='unique']")]
         [TestCase("<a><b /><b /><b /><b><c id='same' type='unique'/><c id='same' /></b></a>", 5, "/a/b/c[@type='unique']")]
         [TestCase("<a><b id='same'><c id='same' /></b><b id='same'><c id='same' /></b><b id='unique'><c id='same' /></b></a>", 6, "/a/b[@id='unique']/c")]
-        public void IdAttributeIsUsedInDistinctPath(string xml, int testElementIndex, string expectedXPath)
+        public void PreferredAttributeIsUsedInDistinctPath(string xml, int testElementIndex, string expectedXPath)
         {
             // Arrange
             XElement element = XElement.Parse(xml);
