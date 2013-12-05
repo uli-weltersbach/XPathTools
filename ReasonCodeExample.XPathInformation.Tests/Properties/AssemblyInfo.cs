b5b94e196c30ff8c942a45e6d802609f0a577cf8
Uli Weltersbach <uli.weltersbach@gmail.com>
2013-12-05 20:31:55 +0100

    Changed version number to 2.0.

--- ReasonCodeExample.XPathInformation.Tests/Properties/AssemblyInfo.cs
+++ ReasonCodeExample.XPathInformation.Tests/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompany("Reason→Code→Example (http://reasoncodeexample.com)")]
 [assembly: AssemblyProduct("ReasonCodeExample.XPathInformation.Tests")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.2")]
-[assembly: AssemblyFileVersion("1.2")]
+[assembly: AssemblyVersion("2.0")]
+[assembly: AssemblyFileVersion("2.0")]
