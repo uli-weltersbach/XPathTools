dfa1c5467df0c0d563df0e504407ff526b8f33e4
Uli Weltersbach <uli.weltersbach@gmail.com>
2014-04-13 13:11:07 +0200

    Display names added

--- ReasonCodeExample.XPathInformation/VisualStudioIntegration/Configuration/XPathSetting.cs
+++ ReasonCodeExample.XPathInformation/VisualStudioIntegration/Configuration/XPathSetting.cs
@@ -1,29 +1,52 @@
-﻿namespace ReasonCodeExample.XPathInformation.VisualStudioIntegration.Configuration
+﻿using System.ComponentModel;
+using System.Text;
+
+namespace ReasonCodeExample.XPathInformation.VisualStudioIntegration.Configuration
 {
     public class XPathSetting
     {
+        [DisplayName("Element name")]
         public string ElementName
         {
             get;
             set;
         }
 
+        [DisplayName("Element XML namespace")]
         public string ElementNamespace
         {
             get;
             set;
         }
 
+        [DisplayName("Attribute name")]
         public string AttributeName
         {
             get;
             set;
         }
 
+        [DisplayName("Attribute XML namespace")]
         public string AttributeNamespace
         {
             get;
             set;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder text = new StringBuilder();
+            if (!string.IsNullOrEmpty(ElementNamespace))
+                text.AppendFormat("{{{0}}}", ElementNamespace);
+            if (!string.IsNullOrEmpty(ElementName))
+                text.Append(ElementName);
+            if (!string.IsNullOrEmpty(AttributeNamespace) || !string.IsNullOrEmpty(AttributeName))
+                text.Append("/@");
+            if (!string.IsNullOrEmpty(AttributeNamespace))
+                text.AppendFormat("{{{0}}}", AttributeNamespace);
+            if (!string.IsNullOrEmpty(AttributeName))
+                text.Append(AttributeName);
+            return text.ToString();
         }
     }
 }
