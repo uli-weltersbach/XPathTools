ee5f6202737582d33c7774ad3154d3373eb8814d
Uli Weltersbach <uli.weltersbach@gmail.com>
2014-04-18 17:21:20 +0200

    Additional test case added to check unnamed XMLNS mixed with identically named elements in document default XMLNS.

--- ReasonCodeExample.XPathInformation.Tests/Writers/AbsoluteXPathWriterTests.cs
+++ ReasonCodeExample.XPathInformation.Tests/Writers/AbsoluteXPathWriterTests.cs
@@ -16,8 +16,9 @@ namespace ReasonCodeExample.XPathInformation.Tests.Writers
         [TestCase("<ns:a xmlns:ns=\"-\" w=\"1\"><ns:b x=\"1\" y=\"1\"><c d=\"value\" /><c d=\"hello\" /><c d=\"world\" /><c ns:d=\"!\" /><c /></ns:b></ns:a>", "/ns:a[1][@w='1']/ns:b[1][@x='1'][@y='1']/c[4][@ns:d='!']/@ns:d")]
         [TestCase("<ns:a xmlns:ns=\"-\" ns:w=\"1\" />", "/ns:a[1][@ns:w='1']")]
         [TestCase("<ns:a xmlns:ns=\"-\" ns:w=\"1\" />", "/ns:a[1][@ns:w='1']/@ns:w")]
-        [TestCase("<r><a xmlns=\"no-prefix\" w=\"1\" /><a xmlns=\"no-prefix\" w=\"1\" /><a xmlns=\"no-prefix\" w=\"1\" /></r>", "/r[1]/*[local-name()='a'][namespace-uri()='no-prefix'][2][@w='1']")]
-        [TestCase("<r><a xmlns=\"no-prefix\" w=\"1\" /><a xmlns=\"no-prefix\" w=\"1\" /><a xmlns=\"no-prefix\" w=\"1\" /></r>", "/r[1]/*[local-name()='a'][namespace-uri()='no-prefix'][2][@w='1']/@w")]
+        [TestCase("<r><a /><a /><a xmlns=\"no-prefix\" w=\"1\" /><a xmlns=\"no-prefix\" w=\"1\" /><a xmlns=\"no-prefix\" w=\"1\" /></r>", "/r[1]/a[2]")]
+        [TestCase("<r><a /><a /><a xmlns=\"no-prefix\" w=\"1\" /><a xmlns=\"no-prefix\" w=\"1\" /><a xmlns=\"no-prefix\" w=\"1\" /></r>", "/r[1]/*[local-name()='a'][namespace-uri()='no-prefix'][2][@w='1']")]
+        [TestCase("<r><a /><a /><a xmlns=\"no-prefix\" w=\"1\" /><a xmlns=\"no-prefix\" w=\"1\" /><a xmlns=\"no-prefix\" w=\"1\" /></r>", "/r[1]/*[local-name()='a'][namespace-uri()='no-prefix'][2][@w='1']/@w")]
         public void WriteOutputIsValid(string xml, string expectedXPath)
         {
             // Arrange
