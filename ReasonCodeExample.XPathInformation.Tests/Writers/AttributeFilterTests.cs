98073ae8216bbadb2e3cc7423e226733b66404a9
Uli Weltersbach <uli.weltersbach@gmail.com>
2014-04-21 13:19:07 +0200

    AttributeFilter implemented

--- ReasonCodeExample.XPathInformation.Tests/Writers/AttributeFilterTests.cs
+++ ReasonCodeExample.XPathInformation.Tests/Writers/AttributeFilterTests.cs
@@ -7,24 +7,36 @@ namespace ReasonCodeExample.XPathInformation.Tests.Writers
     [TestFixture]
     public class AttributeFilterTests
     {
-        [TestCase("id", "")]
-        [TestCase("name", "")]
-        [TestCase("type", "")]
-        [TestCase("id", "a")]
-        [TestCase("name", "b")]
-        [TestCase("type", "c")]
-        public void AttributeNameFiltering(string attributeName, string attributeNamespace)
+        [TestCase("a", null, "e", null, true)]
+        [TestCase("a", null, "e", "X", false)]
+        [TestCase("a", "X", "e", null, false)]
+        [TestCase("a", "X", "e", "X", false)]
+        [TestCase("a", "a-xmlns", "e", "e-xmlns", true)]
+        [TestCase("a", "a-xmlns", "e", null, true)]
+        [TestCase("a", null, "e", "e-xmlns", true)]
+        public void NameAndNamespaceFiltering(string attributeName, string attributeNamespace, string elementName, string elementNamespace, bool expectedResult)
         {
             // Arrange
-            XAttribute attribute = new XAttribute(XName.Get(attributeName, attributeNamespace), string.Empty);
-            var settings = new[] { new XPathSetting { AttributeName = attributeName, AttributeNamespace = attributeNamespace} };
+            var attribute = new XAttribute(XName.Get("a", "a-xmlns"), string.Empty);
+            var element = new XElement(XName.Get("e", "e-xmlns"));
+            element.Add(attribute);
+            var settings = new[]
+                           {
+                               new XPathSetting
+                               {
+                                   AttributeName = attributeName,
+                                   AttributeNamespace = attributeNamespace,
+                                   ElementName = elementName,
+                                   ElementNamespace = elementNamespace
+                               }
+                           };
             var filter = new AttributeFilter(settings);
 
             // Act
             var isIncluded = filter.IsIncluded(attribute);
 
             // Assert
-            Assert.That(isIncluded, Is.True);
+            Assert.That(isIncluded, Is.EqualTo(expectedResult));
         }
     }
 }
