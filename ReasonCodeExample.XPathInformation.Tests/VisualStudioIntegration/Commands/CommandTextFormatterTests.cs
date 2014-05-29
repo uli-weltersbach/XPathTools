e6c3283a90bba695e69eac2f1c241489fcb37341
Uli Weltersbach <uli.weltersbach@gmail.com>
2014-05-29 10:48:31 +0200

    CommandTextFormatter and tests added.

---
+++ ReasonCodeExample.XPathInformation.Tests/VisualStudioIntegration/Commands/CommandTextFormatterTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using ReasonCodeExample.XPathInformation.VisualStudioIntegration.Commands;
+
+namespace ReasonCodeExample.XPathInformation.Tests.VisualStudioIntegration.Commands
+{
+    [TestFixture]
+    public class CommandTextFormatterTests
+    {
+        [TestCase("text", 1, "(1 match) text")]
+        [TestCase("text", 2, "(2 matches) text")]
+        [TestCase("abcdefghijklmnopqrstuvwxyz", 2, "(2 matches) ...qrstuvwxyz")]
+        public void EllipsisIsPrependedCorrectly(string commandText, int elementCount, string expectedCommandText)
+        {
+            // Arrange
+            CommandTextFormatter.MaxLength = 10;
+
+            // Act
+            var output = CommandTextFormatter.Format(commandText, elementCount);
+
+            // Assert
+            Assert.That(output, Is.EqualTo(expectedCommandText));
+        }
+    }
+}
---
+++ ReasonCodeExample.XPathInformation/VisualStudioIntegration/Commands/CommandTextFormatter.cs
@@ -0,0 +1,24 @@
+﻿namespace ReasonCodeExample.XPathInformation.VisualStudioIntegration.Commands
+{
+    internal static class CommandTextFormatter
+    {
+        static CommandTextFormatter()
+        {
+            MaxLength = 80;
+        }
+
+        public static int MaxLength
+        {
+            get;
+            set;
+        }
+
+        public static string Format(string xpath, int elementCount)
+        {
+            var excessChars = xpath.Length - MaxLength;
+            var commandText = excessChars > 0 ? "..." + xpath.Substring(excessChars, MaxLength) : xpath;
+            var matchText = elementCount == 1 ? "match" : "matches";
+            return string.Format("({0} {1}) {2}", elementCount, matchText, commandText);
+        }
+    }
+}
