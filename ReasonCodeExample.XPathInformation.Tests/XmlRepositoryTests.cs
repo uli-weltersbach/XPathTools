fcf5ff80272d4f7338f380422ce68f65bb378510
Uli Weltersbach <uli.weltersbach@gmail.com>
2017-04-05 00:19:15 +0200

    DTD test refactored to verify relative file paths are supported.

--- ReasonCodeExample.XPathInformation.Tests/XmlRepositoryTests.cs
+++ ReasonCodeExample.XPathInformation.Tests/XmlRepositoryTests.cs
@@ -176,17 +176,236 @@ namespace ReasonCodeExample.XPathInformation.Tests
         public void HandlesDtdReferencesGracefully()
         {
             // Arrange
-            var dtd = "<!ENTITY lock \"warlock\">";
-            var dtdFileName = Path.GetTempFileName();
-            File.WriteAllText(dtdFileName, dtd);
-            var xml = $"<?xml version=\"1.0\" encoding=\"utf-8\"?><!DOCTYPE database SYSTEM \"{dtdFileName}\"><class>&lock;</class>";
+            var xmlFilePath = Path.GetTempFileName();
+            File.WriteAllText(xmlFilePath, XML);
+
+            var dtdFilePath = Path.GetTempFileName();
+            File.WriteAllText(dtdFilePath, DTD);
+            var dtdFileName = Path.GetFileName(dtdFilePath);
+
+            var xml = File.ReadAllText(xmlFilePath).Replace("<DTD FILE NAME>", dtdFileName);
             var repository = new XmlRepository();
 
             // Act
-            repository.LoadXml(xml);
+            repository.LoadXml(xml, new System.Uri(xmlFilePath));
 
             // Assert
             Assert.That(repository.GetRootElement(), Is.Not.Null);
         }
+
+        #region XML
+        private const string XML = @"<?xml version=""1.0"" encoding=""utf-8""?>
+<!DOCTYPE database SYSTEM ""<DTD FILE NAME>"">
+<database>
+  <server id=""v12"" lang=""cz"">
+    <continent name=""Kalimdor"">
+      <player id=""v12354"">
+        <name>Bimbinbiribong</name>
+        <class>&lock;</class>
+        <level>75</level>
+        <health>123</health>
+        <mana>155</mana>
+        <attributes>
+          <strength>12</strength>
+          <agility>10</agility>
+          <stamina>15</stamina>
+          <intellect>16</intellect>
+          <spirit>20</spirit>
+          <armor>67</armor>
+        </attributes>
+        <buffs>
+          <buff id=""u1532"">
+            <name>Power Word: Fortitude</name>
+            <duration timeUnit=""minute"">60</duration>
+            <description>
+              Gives<highlight>2 stamina</highlight> to owner.
+            </description>
+          </buff>
+        </buffs>
+        <debuffs/>
+        <location>
+          <area>Elwynn Forest</area>
+          <city>Stormwind City</city>
+        </location>
+        <icon src=""blablablablabla.jpg""/>
+      </player>
+      <player id=""v154"">
+        <name>Charles Xavier</name>
+        <class>hunter</class>
+        <level>70</level>
+        <health>123</health>
+        <mana>155</mana>
+        <attributes>
+          <strength>12</strength>
+          <agility>10</agility>
+          <stamina>15</stamina>
+          <intellect>16</intellect>
+          <spirit>20</spirit>
+          <armor>67</armor>
+        </attributes>
+        <buffs>
+          <buff id=""u153"">
+            <name>Divine Shield</name>
+            <duration timeUnit=""second"">12</duration>
+            <description>Makes you invincible for given duration.</description>
+          </buff>
+          <buff id=""a123"">
+            <name>Seal of Righteousness</name>
+            <duration timeUnit=""second"">20</duration>
+            <description>Makes you hit by 124 dmg more for every hit.</description>
+          </buff>
+        </buffs>
+        <debuffs>
+          <debuff id=""f222"">
+            <name>Corruption</name>
+            <duration timeUnit=""second"">22</duration>
+            <description>Deals 222 dmg every 3 seconds.</description>
+          </debuff>
+        </debuffs>
+        <location>
+          <area>Elwynn Forest</area>
+          <city>Stormwind City</city>
+        </location>
+        <icon src=""blablablablabla.jpg""/>
+      </player>
+      <pet id=""a95"" ownerID=""v12354"">
+        <name>Lakatos</name>
+        <type>Imp</type>
+      </pet>
+      <pet id=""u95"" ownerID=""v12354"">
+        <name>Laco</name>
+        <type>Imp</type>
+      </pet>
+      <player id=""v1549"">
+        <name>KingKong</name>
+        <class>hunter</class>
+        <level>70</level>
+        <health>111</health>
+        <mana>222</mana>
+        <attributes>
+          <strength>8</strength>
+          <agility>9</agility>
+          <stamina>18</stamina>
+          <intellect>19</intellect>
+          <spirit>10</spirit>
+          <armor>100</armor>
+        </attributes>
+        <buffs/>
+        <debuffs/>
+        <location>
+          <area>Elwynn Forest</area>
+          <city>Stormwind City</city>
+        </location>
+        <icon src=""blablablablabla.jpg""/>
+      </player>
+    </continent>
+  </server>
+  <server id=""v13"" lang=""cz"">
+    <continent name=""Azeroth"">
+      <player id=""v12359"">
+        <name>Uff</name>
+        <class>warlock</class>
+        <level>75</level>
+        <health>123</health>
+        <mana>155</mana>
+        <attributes>
+          <strength>9</strength>
+          <agility>10</agility>
+          <stamina>15</stamina>
+          <intellect>16</intellect>
+          <spirit>20</spirit>
+          <armor>67</armor>
+        </attributes>
+        <buffs/>
+        <debuffs>
+          <debuff id=""u15"">
+            <name>Shadow Word: Pain</name>
+            <duration timeUnit=""second"">21</duration>
+            <description>
+              Gives<highlight>2 damage</highlight> every 3 seconds.
+            </description>
+          </debuff>
+        </debuffs>
+        <location>
+          <area>Elwynn Forest</area>
+          <city>Stormwind City</city>
+        </location>
+        <icon src=""blablablablabla.jpg""/>
+      </player>
+      <pet id=""a915"" ownerID=""v12354"">
+        <name>Lakatos</name>
+        <type>Imp</type>
+      </pet>
+      <pet id=""b915"" ownerID=""v12359"">
+        <name>Lakatos</name>
+        <type>Imp</type>
+      </pet>
+      <pet id=""c915"" ownerID=""v12359"">
+        <name>Lakatos</name>
+        <type>Imp</type>
+      </pet>
+    </continent>
+  </server>
+  <appendix>
+    <![CDATA[
+document.writeln(""All rights to me and Blizzard"");
+  ]]>
+  </appendix>
+  <!-- date when it was generated  -->
+  <date>
+    <?php echo ""Generated at"" . date('l jS \of F Y h:i:s A') ?>
+  </date>
+</database>";
+        #endregion
+
+        #region DTD
+        private const string DTD = @"<!ELEMENT database (server*, appendix?, date?)>
+<!ELEMENT appendix ANY>
+<!ELEMENT server (continent+ )>
+<!ELEMENT date (#PCDATA)>
+<!ATTLIST server
+id CDATA #REQUIRED
+lang (en | cz | de | sp) ""en"">
+<!ELEMENT continent (player | gameObject | pet)*>
+<!ATTLIST continent name (Azeroth | Outland) #REQUIRED>
+<!ELEMENT player (name , class , level , health , mana , attributes , buffs , debuffs , location, icon)>
+<!ATTLIST player id ID #REQUIRED>
+<!ELEMENT health (#PCDATA)>
+<!ELEMENT mana (#PCDATA)>
+<!ELEMENT name (#PCDATA)>
+<!ELEMENT class (#PCDATA)>
+<!ELEMENT level (#PCDATA)>
+<!ELEMENT buffs ( buff)*>
+<!ELEMENT debuffs (debuff)*>
+<!ELEMENT buff (name , duration , description)>
+<!ATTLIST buff id ID #REQUIRED>
+<!ELEMENT duration (#PCDATA)>
+<!ATTLIST duration timeUnit (second | minute | hour) ""second"">
+<!ELEMENT description (#PCDATA | highlight)*>
+<!ELEMENT highlight (#PCDATA)>
+<!ELEMENT debuff (name , duration , description)>
+<!ATTLIST debuff id ID #REQUIRED>
+<!ELEMENT attributes (strength , agility , stamina , intellect , spirit , armor)>
+<!ELEMENT strength (#PCDATA)>
+<!ELEMENT stamina (#PCDATA)>
+<!ELEMENT agility (#PCDATA)>
+<!ELEMENT intellect (#PCDATA)>
+<!ELEMENT spirit (#PCDATA)>
+<!ELEMENT armor (#PCDATA)>
+<!ELEMENT gameObject (type)>
+<!ATTLIST gameObject id ID #REQUIRED>
+<!ELEMENT type (#PCDATA)>
+<!ELEMENT location (area , city?)>
+<!ELEMENT area (#PCDATA)>
+<!ELEMENT city (#PCDATA)>
+<!ELEMENT pet (name , type)>
+<!ATTLIST pet 
+id ID #REQUIRED
+ownerID IDREF #REQUIRED>
+<!ENTITY lock ""warlock"">
+<!ENTITY war ""warrior"">
+<!ELEMENT icon EMPTY>
+<!ATTLIST icon src CDATA #REQUIRED>";
+        #endregion
     }
 }
