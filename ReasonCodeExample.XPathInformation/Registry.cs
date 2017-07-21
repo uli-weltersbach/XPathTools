37b72d61df6162bfc533e8d201da2414c4da5fe4
Uli Weltersbach <uw@pentia.dk>
2017-07-21 10:37:13 +0200

    Registry and ServiceContainer split into separate files

--- ReasonCodeExample.XPathInformation/Registry.cs
+++ ReasonCodeExample.XPathInformation/Registry.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace ReasonCodeExample.XPathInformation
+﻿namespace ReasonCodeExample.XPathInformation
 {
     internal static class Registry
     {
@@ -13,26 +10,6 @@ namespace ReasonCodeExample.XPathInformation
         public static ServiceContainer Current
         {
             get;
-            private set;
-        }
-    }
-
-    internal class ServiceContainer
-    {
-        private readonly IDictionary<Type, object> _services = new Dictionary<Type, object>();
-
-        public void Set<T>(T instance)
-        {
-            _services.Add(typeof(T), instance);
-        }
-
-        public T Get<T>()
-        {
-            if(_services.ContainsKey(typeof(T)))
-            {
-                return (T)_services[typeof(T)];
-            }
-            return default(T);
         }
     }
 }
---
+++ ReasonCodeExample.XPathInformation/ServiceContainer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReasonCodeExample.XPathInformation
+{
+  internal class ServiceContainer
+  {
+    private readonly IDictionary<Type, object> _services = new Dictionary<Type, object>();
+
+    public void Set<T>(T instance)
+    {
+      _services.Add(typeof(T), instance);
+    }
+
+    public T Get<T>()
+    {
+      if(_services.ContainsKey(typeof(T)))
+      {
+        return (T)_services[typeof(T)];
+      }
+      return default(T);
+    }
+  }
+}
