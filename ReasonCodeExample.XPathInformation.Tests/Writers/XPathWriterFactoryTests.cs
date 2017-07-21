f33966ac895411628351c97fa1e81c13a1eb6e3b
Uli Weltersbach <uw@pentia.dk>
2017-07-21 15:13:39 +0200

    XPathWriterFactory implemented
    This should reduce code duplication for creational logic.

---
+++ ReasonCodeExample.XPathInformation.Tests/Writers/XPathWriterFactoryTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NSubstitute;
+using NUnit.Framework;
+using ReasonCodeExample.XPathInformation.Writers;
+
+namespace ReasonCodeExample.XPathInformation.Tests.Writers
+{
+    public class XPathWriterFactoryTests
+    {
+        [TestCase("Generic", typeof(XPathWriter))]
+        [TestCase("Absolute", typeof(AbsoluteXPathWriter))]
+        [TestCase("Distinct", typeof(XPathWriter))]
+        [TestCase("Simplified", typeof(SimplifiedXPathWriter))]
+        public void CanCreateXPathWriterFromFriendlyName(string xpathWriterFriendlyName, Type expectedXPathWriterType)
+        {
+            // Arrange
+            var configuration = Substitute.For<IConfiguration>();
+            var factory = new XPathWriterFactory(configuration);
+
+            // Act
+            var writer = factory.CreateFromFriendlyName(xpathWriterFriendlyName);
+
+            // Assert
+            Assert.That(writer, Is.Not.Null.And.TypeOf(expectedXPathWriterType));
+        }
+    }
+}
---
+++ ReasonCodeExample.XPathInformation/Writers/XPathWriterFactory.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using ReasonCodeExample.XPathInformation.VisualStudioIntegration;
+
+namespace ReasonCodeExample.XPathInformation.Writers
+{
+    internal class XPathWriterFactory
+    {
+        private readonly IConfiguration _configuration;
+
+        public XPathWriterFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public IWriter CreateFromFriendlyName(string xpathWriterFriendlyName)
+        {
+            switch(xpathWriterFriendlyName)
+            {
+                case "Generic":
+                    return CreateFromCommandId(Symbols.CommandIDs.CopyGenericXPath);
+
+                case "Absolute":
+                    return CreateFromCommandId(Symbols.CommandIDs.CopyAbsoluteXPath);
+
+                case "Distinct":
+                    return CreateFromCommandId(Symbols.CommandIDs.CopyDistinctXPath);
+
+                case "Simplified":
+                    return CreateFromCommandId(Symbols.CommandIDs.CopySimplifiedXPath);
+
+                default:
+                    throw new ArgumentException($"Unsupported XPath writer friendly name '{xpathWriterFriendlyName}'.");
+            }
+        }
+
+        public IWriter CreateFromCommandId(int xpathWriterCommandId)
+        {
+            switch(xpathWriterCommandId)
+            {
+                case Symbols.CommandIDs.CopyGenericXPath:
+                    return new XPathWriter(new[] {new AttributeFilter(_configuration.AlwaysDisplayedAttributes)});
+
+                case Symbols.CommandIDs.CopyAbsoluteXPath:
+                    return new AbsoluteXPathWriter(new[] {new AttributeFilter(_configuration.AlwaysDisplayedAttributes)});
+
+                case Symbols.CommandIDs.CopyDistinctXPath:
+                    return new XPathWriter(new[] { new AttributeFilter(_configuration.AlwaysDisplayedAttributes), new DistinctAttributeFilter(_configuration.PreferredAttributeCandidates) });
+
+                case Symbols.CommandIDs.CopySimplifiedXPath:
+                    return new SimplifiedXPathWriter(new[] {new AttributeFilter(_configuration.AlwaysDisplayedAttributes)});
+
+                default:
+                    throw new ArgumentException($"Unsupported XPath writer ID '{xpathWriterCommandId}'.");
+            }
+        }
+    }
+}
