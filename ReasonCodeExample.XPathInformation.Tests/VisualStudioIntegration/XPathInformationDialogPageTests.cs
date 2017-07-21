58b94ac4cc6eb0768ffd2177f09c60042ce340a4
Uli Weltersbach <uw@pentia.dk>
2017-07-21 10:58:59 +0200

    Failing test added as entrypoint for future development

---
+++ ReasonCodeExample.XPathInformation.Tests/VisualStudioIntegration/XPathInformationDialogPageTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace ReasonCodeExample.XPathInformation.Tests.VisualStudioIntegration
+{
+  public class XPathInformationDialogPageTests
+  {
+    [Test]
+    public void CanDeserializeStatusbarXPathWriterFromSetting()
+    {
+      // Arrange
+
+      // Act
+
+      // Assert
+      Assert.Fail("Not implemented");
+    }
+  }
+}
