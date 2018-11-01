89adcba184864be5851ded3ce5225d5c0caec2c1
Uli <uli.weltersbach@gmail.com>
2018-11-01 03:52:14 +0100

    HTTP XML resolving is now suppressed
    Fixes #30

---
+++ ReasonCodeExample.XPathTools/FileXmlUrlResolver.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ReasonCodeExample.XPathTools
+{
+    /// <summary>
+    /// A <c>XmlUrlResolver</c> which supports *only* file lookup.
+    /// This avoids timeouts due to slow or blocked HTTP requests, 
+    /// while still solving all currently supported use cases - 
+    /// presumably because XML validation isn't currently on the list of use cases.
+    /// </summary>
+    /// <remarks>
+    /// See the following resources for further discussion:
+    /// https://docs.microsoft.com/en-us/dotnet/api/system.xml.xmlurlresolver?view=netframework-4.7.2
+    /// https://stackoverflow.com/questions/2558021/an-error-has-occurred-opening-extern-dtd-w3-org-xhtml1-transitional-dtd-503
+    /// https://msdn.microsoft.com/en-us/library/aa302284.aspx
+    /// </remarks>
+    internal class FileXmlUrlResolver : XmlUrlResolver
+    {
+        public override Uri ResolveUri(Uri baseUri, string relativeUri)
+        {
+            return base.ResolveUri(baseUri, relativeUri);
+        }
+
+        public override bool SupportsType(Uri absoluteUri, Type type)
+        {
+            return base.SupportsType(absoluteUri, type);
+        }
+
+        public override object GetEntity(Uri absoluteUri, string role, Type ofObjectToReturn)
+        {
+            if (absoluteUri == null)
+            {
+                throw new ArgumentNullException(nameof(absoluteUri));
+            }
+            if (absoluteUri.Scheme.StartsWith("file", StringComparison.InvariantCultureIgnoreCase))
+            {
+                return base.GetEntity(absoluteUri, role, ofObjectToReturn);
+            }
+            else
+            {
+                throw new NotSupportedException();
+            }
+        }
+
+        public override Task<object> GetEntityAsync(Uri absoluteUri, string role, Type ofObjectToReturn)
+        {
+            return new Task<object>(() => GetEntity(absoluteUri, role, ofObjectToReturn));
+        }
+    }
+}
