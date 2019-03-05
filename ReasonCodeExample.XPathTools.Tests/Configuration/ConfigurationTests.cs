609fc65fe53d95667b4762e0e68c0fe8742c3e73
Uli <uli.weltersbach@gmail.com>
2019-03-05 13:25:16 +0100

    Begun implementing configuration end-to-end tests

--- ReasonCodeExample.XPathTools.Tests/Configuration/ConfigurationTests.cs
+++ ReasonCodeExample.XPathTools.Tests/Configuration/ConfigurationTests.cs
@@ -1,49 +1,50 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.Design;
+using System.Xml.Linq;
 using Microsoft.VisualStudio.Shell.Interop;
 using NSubstitute;
 using NUnit.Framework;
 using ReasonCodeExample.XPathTools.Configuration;
 using ReasonCodeExample.XPathTools.Statusbar;
+using ReasonCodeExample.XPathTools.Tests.VisualStudioIntegration;
+using ReasonCodeExample.XPathTools.Tests.Workbench;
 using ReasonCodeExample.XPathTools.VisualStudioIntegration;
 
 namespace ReasonCodeExample.XPathTools.Tests.Configuration
 {
+    [TestFixture]
     [Category("Integration")]
     public class ConfigurationTests
     {
+        private readonly VisualStudioExperimentalInstance _instance = new VisualStudioExperimentalInstance();
+
+        [OneTimeSetUp]
+        public void StartVisualStudio()
+        {
+            _instance.ReStart();
+        }
+
+        [OneTimeTearDown]
+        public void StopVisualStudio()
+        {
+            _instance.Stop();
+        }
+        
         [TestCase(XPathFormat.Generic, "<a><b id='hello'><c/></b></a>", 19, "/a/b[@id='hello']/c")]
         [TestCase(XPathFormat.Absolute, "<a id='world'><b><c/></b></a>", 19, "/a[1][@id='world']/b[1]/c[1]")]
         public void StatusbarXPathFormatChangesWhenConfigurationIsChanged(XPathFormat xpathFormat, string xml, int xmlElementIndex, string expectedXPath)
         {
             // Arrange
-            var serviceContainer = Registry.Current;
-
-            // Set up the configuration to a default XPathFormat
-            var configuration = Substitute.For<IConfiguration>();
-            configuration.StatusbarXPathFormat.ReturnsForAnyArgs(XPathFormat.Simplified);
-            // Define an XML attribute which should always be shown in the statusbar XPath
-            configuration.AlwaysDisplayedAttributes.ReturnsForAnyArgs(new List<XPathSetting>{new XPathSetting{AttributeName = "id"}});
-            configuration.PreferredAttributeCandidates.ReturnsForAnyArgs(new List<XPathSetting>());
-
-            // Initialize all services, incl. the statusbar adapter
-            var menuCommandService = Substitute.For<IMenuCommandService>();
-            var statusbar = Substitute.For<IVsStatusbar>();
-
-            // Set up the XML repo initialized by the package
-            var xmlRepository = serviceContainer.Get<XmlRepository>();
-            xmlRepository.LoadXml(xml, null);
-            var selectedElement = xmlRepository.GetElement(xmlRepository.GetRootElement(), 1, xmlElementIndex);
-            xmlRepository.SetSelected(selectedElement);
+            _instance.OpenXmlFile(xml, xmlElementIndex);
+            var configuration = new XPathToolsDialogPageAutomationModel(_instance.MainWindow);
             
-            var statusbarAdapter = serviceContainer.Get<StatusbarAdapter>();
-
             // Act
-            configuration.StatusbarXPathFormat.ReturnsForAnyArgs(xpathFormat);
-            statusbarAdapter.SetText(null, null);
+            configuration.SetStatusbarXPathFormat(xpathFormat);
 
             // Assert
-            statusbar.Received().SetText(Arg.Is(expectedXPath));
+            throw new NotImplementedException();
+            // CHECK THAT STATUSBAR TEXT MATCHES "expectedXPath"
         }
     }
 }
---
+++ ReasonCodeExample.XPathTools.Tests/Configuration/XPathToolsDialogPageAutomationModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Automation;
+using ReasonCodeExample.XPathTools.VisualStudioIntegration;
+
+namespace ReasonCodeExample.XPathTools.Tests.Configuration
+{
+    public class XPathToolsDialogPageAutomationModel
+    {
+        private readonly AutomationElement _mainWindow;
+
+        public XPathToolsDialogPageAutomationModel(AutomationElement mainWindow)
+        {
+            _mainWindow = mainWindow;
+        }
+
+        public bool IsOpen
+        {
+            get;
+        }
+
+        public void Open()
+        {
+
+        }
+
+        public void Close()
+        {
+
+        }
+
+        public void SetStatusbarXPathFormat(XPathFormat format)
+        {
+
+        }
+    }
+}
