59d1363c7b397465a6ef95725a337729064ba32c
Uli Weltersbach <uw@pentia.dk>
2019-03-07 23:00:21 +0100

    Tests successful

--- ReasonCodeExample.XPathTools.Tests/Configuration/ConfigurationTests.cs
+++ ReasonCodeExample.XPathTools.Tests/Configuration/ConfigurationTests.cs
@@ -25,9 +25,9 @@ namespace ReasonCodeExample.XPathTools.Tests.Configuration
             _visualStudio.Stop();
         }
         
-        [TestCase(XPathFormat.Generic, "<a><b id='hello'><c/></b></a>", 19, "/a/b/c")]
-        [TestCase(XPathFormat.Absolute, "<a id='world'><b><c/></b></a>", 19, "/a[1]/b[1]/c[1]")]
-        [TestCase(XPathFormat.Distinct, "<a><b id='hello'><c/></b></a>", 19, "/a/b[@id='hello']/c")]
+        [TestCase(XPathFormat.Generic, "<a><b id='hello'><c/></b><b id='world'><c/></b></a>", 41, "/a/b/c")]
+        [TestCase(XPathFormat.Absolute, "<a><b id='hello'><c/></b><b id='world'><c/></b></a>", 41, "/a[1]/b[2]/c[1]")]
+        [TestCase(XPathFormat.Distinct, "<a><b id='hello'><c/></b><b id='world'><c/></b></a>", 41, "/a/b[@id='world']/c")]
         public void StatusbarXPathFormatChangesWhenConfigurationIsChanged(XPathFormat xpathFormat, string xml, int xmlElementIndex, string expectedXPath)
         {
             // Arrange
